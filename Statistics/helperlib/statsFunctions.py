1f051d7c36626fdcc404d4376be80f2de6f766d0
Rohit Nag <rnag2018@gmail.com>
2022-10-18 13:11:26 +0100

    Finished st1.1 & 1.2

---
+++ Statistics/helperlib/statsFunctions.py
@@ -0,0 +1,33 @@
+def summarise_data(data, name='Data'):
+    print(f'{name} summary:')
+
+    location_dict = {
+        'mean': data.mean(),
+        'median': data.median(),
+        'lowerq': data.quantile(0.25),
+        'upperq': data.quantile(0.75),
+        'min': max(data),
+        'max': min(data)
+    }
+
+    dispersion_dict = {
+        'range': location_dict['max'] - location_dict['min'],
+        'iqr': location_dict['upperq'] - location_dict['lowerq'],
+        'var': data.var(),
+        'std': data.std()
+    }
+
+    skewness_dict = {
+        'skew': data.skew(),
+        'kurtosis': data.kurt()
+    }
+
+    summary_dict = {
+        'location': location_dict,
+        'dispersion': dispersion_dict,
+        'skewness': skewness_dict
+    }
+
+    for key in summary_dict:
+        print(f' {key}: ')
+        print(f'  {summary_dict[key]}')
