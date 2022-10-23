02cbfd754c359fd0a11612e2262ef61b5144ce75
Rohit Nag <rnag2018@gmail.com>
2022-10-23 11:53:28 +0100

    Finished ml3.1

--- MachineLearning/helperlib/arrayfunctions.py
+++ MachineLearning/helperlib/arrayfunctions.py
@@ -7,3 +7,8 @@ def get_grid_points(npx=200, npy=200, limits=(-1, 1)):
     grid = np.array(np.meshgrid(x_axis, y_axis))
     grid_points = grid.reshape([2, npx * npy]).T
     return grid_points, x_axis, y_axis
+
+
+def accuracy(data_predicted, data_test):
+    data_predicted_wrong = np.logical_xor(data_predicted, data_test)
+    return 1 - sum(data_predicted_wrong) / len(data_predicted_wrong)
