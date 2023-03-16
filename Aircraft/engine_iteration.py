1584f5f112add3f05607b495735e43cd61d625da
Rohit Nag <rnag2018@gmail.com>
2023-03-16 08:44:49 +0000

    Added second iterator to engine_iteration

--- Aircraft/engine_iteration.py
+++ Aircraft/engine_iteration.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple, Set
+from typing import Dict, Tuple, Set, Iterable
 import numpy as np
 from src.turbomach_analyser import Engine
 from src.utils import (plots,
@@ -59,10 +59,10 @@ def get_engine_constants():
 
 def __get_variable_ranges():
     return {
-        'engine_diameter': np.linspace(2.5, 3.25, 4, endpoint=True),
+        'engine_diameter': np.linspace(2.5, 3, 3, endpoint=True),
         'lpt_work_coefficient': np.linspace(0.8, 2.4, 5, endpoint=True),
         'hpt_work_coefficient': np.linspace(0.8, 2, 5, endpoint=True),
-        'hpt_angular_velocity': np.linspace(700, 1500, 5, endpoint=True),
+        'hpt_angular_velocity': np.linspace(500, 1500, 5, endpoint=True),
         'min_blade_length': np.linspace(0.012, 0.015, 3, endpoint=True),
         'hpt_min_blade_length': np.linspace(0.012, 0.03, 5, endpoint=True),
         'lpt_min_blade_length': np.linspace(0.012, 0.05, 5, endpoint=True),
@@ -73,81 +73,146 @@ def __get_variable_ranges():
     }
 
 
-def get_number_of_iterations():
-    d = __get_variable_ranges()
-    return int(np.prod([len(v) for v in d.values()]))
+def __get_variable_ranges_from_file(valid_variables_path, per_var_iterations):
+    valid_variables_list = f.read_hashed_file_to_dict_list(
+        valid_variables_path)
+    # Remove the engine score from all dictionaries
+    [valid_variables.pop('engine_score', None)
+     for valid_variables in valid_variables_list]
+    result = {}
+    # Find the minimum and maximum values for each key
+    for d in valid_variables_list:
+        for key, value in d.items():
+            if key not in result:
+                result[key] = {"min": value, "max": value}
+            else:
+                if value < result[key]["min"]:
+                    result[key]["min"] = value
+                if value > result[key]["max"]:
+                    result[key]["max"] = value
+
+    # Create the final dictionary with np.linspace for each key
+    for key, value in result.items():
+        result[key] = np.linspace(
+            value["min"],
+            value["max"],
+            per_var_iterations,
+            endpoint=True)
+        # Convert to set and back to list to remove duplicates
+        result[key] = list(set(result[key]))
+        # Sort the list after removing duplicates
+        result[key].sort()
+    return result
 
 
-def main(tried_vars_dir: str, valid_vars_dir: str, pbar_ncols: int = 150):
-    # Get the constants and variable ranges for the engine
-    consts_kwargs = get_constants()
-    engine_consts_kwargs = get_engine_constants()
-    var_ranges_dict = __get_variable_ranges()
-    # Generate all possible combinations of variables
-    all_possible_vars_dicts = (
-        {k: v for k, v in zip(var_ranges_dict.keys(), p)}
-        for p in itertools.product(*var_ranges_dict.values())
-    )
-    # Get the variable key hash
-    # The hash below is used to identify the variable combinations and is reversible
-    var_key_hash = f.hash_dict_keys(var_ranges_dict)
-    # The hash below is used to name the files and is not reversible
-    var_key_hash_compact = f.compact_hash_dict_keys(var_ranges_dict)
-    # Read the tried and valid variable hashes from the files
-    tried_vars_path = f'{tried_vars_dir}/{var_key_hash_compact}.csv'
-    valid_vars_path = f'{valid_vars_dir}/{var_key_hash_compact}.csv'
+def get_number_of_iterations(range_dict):
+    return int(np.prod([len(v) for v in range_dict.values()]))
+
+
+def generate_possible_var_dicts(var_ranges_dict):
+    return ({k: v for k, v in zip(var_ranges_dict.keys(), p)}
+            for p in itertools.product(*var_ranges_dict.values()))
+
+
+def get_no_iterations(var_ranges_dict):
+    return np.prod([len(x) for x in var_ranges_dict.values()])
+
+
+def read_vars_files(tried_vars_path, valid_vars_path):
     tried_var_key_hash, tried_var_vals_hash_set = f.read_vars_file(
         tried_vars_path)
     valid_var_key_hash, valid_var_vals_hash_set = f.read_vars_file(
         valid_vars_path)
-    # Calculate the total number of iterations
-    no_iterations = get_number_of_iterations()
-    print(f'No of iterations: {no_iterations}')
-    # Initialize the main progress bar for processing iterations
+    return tried_var_key_hash, tried_var_vals_hash_set, valid_var_key_hash, valid_var_vals_hash_set
+
+
+def __run_iteration(no_iterations: int,
+                    all_possible_vars_dicts,
+                    tried_var_vals_hash_set: set,
+                    valid_var_vals_hash_set: set,
+                    tried_var_key_hash: str,
+                    valid_var_key_hash: str,
+                    var_key_hash: str):
     with tqdm(total=no_iterations,
               desc='Processing',
-              unit='var_dict',
-              ncols=pbar_ncols) as pbar:
-        # Initialize the progress bar for valid iterations
+              unit='var_dict',) as pbar:
         with tqdm(total=no_iterations,
                   desc='Accepted  ',
                   unit='var_dict',
                   position=1,
-                  ncols=pbar_ncols,
                   colour='CYAN') as valid_pbar:
-            # Iterate through all possible variable combinations
+            valid_pbar.update(len(valid_var_vals_hash_set))
             for var_dict in all_possible_vars_dicts:
-                # Get the variable value hash
                 var_val_hash = f.hash_dict_vals(var_dict)
-                # Check if the current variable combination is unprocessed or has a different key hash
                 if var_val_hash not in tried_var_vals_hash_set or var_key_hash != tried_var_key_hash:
                     tried_var_vals_hash_set.add(var_val_hash)
                     try:
-                        # Create an engine instance with the current variable combination
-                        engine = Engine(**consts_kwargs,
-                                        **engine_consts_kwargs,
+                        engine = Engine(**get_constants(),
+                                        **get_engine_constants(),
                                         **var_dict)
-                        # If the engine is valid, add the variable value hash with engine score to the valid set and update the valid progress bar
                         if engine.is_valid:
                             valid_var_vals_hash_set.add(
                                 var_val_hash + f',{engine.score}')
                             valid_pbar.update(1)
                     except:
                         pass
-                # Update the main progress bar
                 pbar.update(1)
-    # Update the tried and valid variable key hashes if they differ from the current key hash
     if var_key_hash != tried_var_key_hash:
         tried_var_key_hash = var_key_hash
         valid_var_key_hash = var_key_hash + ',engine_score'
-    # Save the tried and valid variable hashes to the files
+    return tried_var_key_hash, tried_var_vals_hash_set, valid_var_key_hash, valid_var_vals_hash_set
+
+
+def complete_run(tried_vars_dir: str, valid_vars_dir: str, var_ranges_dict, per_var_iterations=None):
+    var_key_hash = f.hash_dict_keys(var_ranges_dict)
+    var_key_hash_compact = f.compact_hash_dict_keys(var_ranges_dict)
+    tried_vars_path = f'{tried_vars_dir}/{var_key_hash_compact}.csv'
+    valid_vars_path = f'{valid_vars_dir}/{var_key_hash_compact}.csv'
+
+    if per_var_iterations is not None:
+        var_ranges_dict = __get_variable_ranges_from_file(
+            valid_vars_path, per_var_iterations)
+
+    all_possible_vars_dicts = generate_possible_var_dicts(var_ranges_dict)
+    tried_var_key_hash, tried_var_vals_hash_set, valid_var_key_hash, valid_var_vals_hash_set = read_vars_files(
+        tried_vars_path, valid_vars_path)
+    no_iterations = get_no_iterations(var_ranges_dict)
+
+    tried_var_key_hash, tried_var_vals_hash_set, valid_var_key_hash, valid_var_vals_hash_set = __run_iteration(
+        no_iterations, all_possible_vars_dicts, tried_var_vals_hash_set, valid_var_vals_hash_set, tried_var_key_hash, valid_var_key_hash, var_key_hash)
+
     f.hashed_vals_to_csv(tried_var_key_hash,
                          tried_var_vals_hash_set,
                          tried_vars_path)
     f.hashed_vals_to_csv(valid_var_key_hash,
                          valid_var_vals_hash_set,
                          valid_vars_path)
-    print(f'\nNo of valid iterations: {len(valid_var_vals_hash_set)}')
+
+    return len(valid_var_vals_hash_set)
+
+
+def first_run(tried_vars_dir: str, valid_vars_dir: str):
+    print('\nRunning first iteration')
+    var_ranges_dict = __get_variable_ranges()
+    no_valid_iterations = complete_run(
+        tried_vars_dir, valid_vars_dir, var_ranges_dict)
+    print(f'No of valid iterations: {no_valid_iterations}')
+    print('Completed first iteration')
+
+
+def second_run(tried_vars_dir: str, valid_vars_dir: str, per_var_iterations: int):
+    print('\nRunning second iteration')
+    var_ranges_dict = __get_variable_ranges()
+    no_valid_iterations = complete_run(
+        tried_vars_dir, valid_vars_dir, var_ranges_dict, per_var_iterations)
+    print(f'No of valid iterations: {no_valid_iterations}')
+    print('Completed second iteration')
+
+
+def main(tried_vars_dir: str, valid_vars_dir: str, second_iteration: False, second_per_var_iterations: int):
+    first_run(tried_vars_dir, valid_vars_dir)
+    if second_iteration:
+        second_run(tried_vars_dir, valid_vars_dir, second_per_var_iterations)
 
 
 if __name__ == '__main__':
@@ -157,7 +222,8 @@ if __name__ == '__main__':
 
     main(tried_variables_dir,
          valid_variables_dir,
-         pbar_ncols=150)
+         second_iteration=True,
+         second_per_var_iterations=10)
 
     et = time.time()
-    print(f'runtime: {f.format_elapsed_time(et - st)}')
+    print(f'\nruntime: {f.format_elapsed_time(et - st)}')
